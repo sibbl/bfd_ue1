5e144355ca5806faaad9a00591015e8e6705fad2
Sebastian Müller <me@sibbl.net>
2014-11-04 10:57:14 +0100

    Konsistent englische Ausgaben...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VoIP
 {
@@ -72,7 +62,7 @@ namespace VoIP
         {
             if (!ValidatePhoneNumber(NumberTextBox.Text))
             {
-                MessageBox.Show("Bitte geben Sie eine gültige Telefonnummer ein!", "Ungültige Nummer",
+                MessageBox.Show("Please enter a valid phone number!", "Invalid phone number",
                     MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
@@ -88,7 +78,7 @@ namespace VoIP
         {
             if (!ValidatePhoneNumber(NumberTextBox.Text))
             {
-                MessageBox.Show("Bitte geben Sie eine gültige Telefonnummer ein!", "Ungültige Nummer",
+                MessageBox.Show("Please enter a valid phone number!", "Invalid phone number",
                     MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
@@ -103,7 +93,7 @@ namespace VoIP
         {
             if (!ValidatePhoneNumber(NumberTextBox.Text))
             {
-                MessageBox.Show("Bitte geben Sie eine gültige Telefonnummer ein!", "Ungültige Nummer",
+                MessageBox.Show("Please enter a valid phone number!", "Invalid phone number",
                     MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
